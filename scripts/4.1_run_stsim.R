f7d5836dd23ed938883afa37030877294db852ad
VLucet <valentin.lucet@gmail.com>
2020-04-23 11:32:31 -0400

    start moving validation of stsim results to different script

--- scripts/4.1_run_stsim.R
+++ scripts/4.1_run_stsim.R
@@ -173,54 +173,5 @@ print("Running StSIM");Sys.time()
 if (STSIM_RUN){
   results <- run(scenario_1_test, summary = TRUE, jobs = OMP_NUM_THREADS)
   print(results)
-  
-  ## DIAGNOSTICS 
-  
-  trans_results <- datasheet(myLibrary, scenario = results$scenarioId, 
-                             "stsim_OutputStratumTransition")
-  # mun_set <- sample(trans_results$SecondaryStratumID, size = 20)
-  
-  print(head(trans_results))
-  
-  targets <- read_csv("config/stsim/TransitionTarget_10y.csv") %>% 
-    #filter(SecondaryStratumID %in% mun_set) %>% 
-    group_by(Timestep, SecondaryStratumID, TransitionGroupID) %>%
-    summarise(Amount_mean = mean(Amount)) %>% ungroup
-  
-  trans_results_mod <- trans_results  %>% 
-    #filter(SecondaryStratumID %in% mun_set) %>% 
-    group_by(Timestep, SecondaryStratumID, TransitionGroupID) %>%
-    summarise(Amount_mean = mean(Amount)) %>% ungroup
-  trans_results_mod_only_3 <- trans_results_mod %>% 
-    filter(Timestep >= 3) %>% 
-    group_by(SecondaryStratumID, TransitionGroupID) %>%
-    summarise(Amount_mean = mean(Amount_mean)) %>% ungroup %>% 
-    mutate(Timestep=3)
-  trans_results_toplot <- bind_rows(subset(trans_results_mod, Timestep < 3), 
-                                    trans_results_mod_only_3)
-  
-  # Plots 
-  trans_results_toplot %>% 
-    filter(TransitionGroupID != "Urbanisation") %>% 
-    ggplot(aes(x=Timestep, y=Amount_mean)) +
-    geom_line(show.legend = F) + 
-    facet_grid_paginate(TransitionGroupID~SecondaryStratumID, nrow=3, ncol=5, page = 1, scales = "free") +
-    geom_line(data=targets, inherit.aes = T, linetype=2)
-  ggsave("outputs/figures/one_to_one_mun.png") 
-  
-  ## JOIN for general diagnoctic plot
-  gen_plot_df <- left_join(trans_results_toplot, targets, 
-                           by= c("Timestep", "SecondaryStratumID", "TransitionGroupID")) %>% 
-    filter(TransitionGroupID != "Urbanisation") %>% 
-    arrange(Timestep)
-  
-  # general plot
-  gen_plot_df %>% 
-    ggplot(aes(x=Amount_mean.x, Amount_mean.y, color=as.factor(Timestep))) +
-    geom_point(show.legend = F) +
-    #geom_abline() +
-    geom_abline(slope = 1, intercept = 0, linetype=2)
-  ggsave("outputs/figures/one_to_one_targets.png")
-  
-  summary(lm(Amount_mean.y~Amount_mean.x, gen_plot_df))
+  saveRDS(results, "data/temp/stsim_run_results.rds")
 }
--- scripts/4.2_validate_stsim_results.R
+++ scripts/4.2_validate_stsim_results.R
@@ -1,1 +1,95 @@
-# TODO
+#-------------------------------------------------------------------------------
+## 4.2 Validate ST-SIM results
+## 2020
+## Inputs: stsim results
+## Outputs: plots and etc..
+#-------------------------------------------------------------------------------
+
+rm(list=ls())
+set.seed(77)
+
+ST_SIM_DIR <- "../../syncrosim_2_10/"
+
+if (is.na(ST_SIM_DIR)) { 
+  ST_SIM_DIR <- "/home/vlucet/Documents/Apex/syncrosim_2_10"
+}
+
+
+#-------------------------------------------------------------------------------
+
+# Load important libraries
+suppressPackageStartupMessages({
+  library(raster)
+  library(tidyverse)
+  library(ggforce)
+  library(rsyncrosim)
+})
+
+# Set options (according to BR)
+options("scipen"=100, "digits"=4)
+
+# Directory paths
+projectDir <- paste0(getwd(), "/")
+dataDir <- paste0(getwd(), "/data/")
+libraryDir <- paste0(getwd(), "/libraries/stsim/")
+
+# STSim library
+mySession <- session(ST_SIM_DIR)
+print(mySession)
+#addPackage("stsim", mySession)
+myLibraryName <- paste0(libraryDir, "monteregie-conncons-scripted.ssim")
+myLibrary <- ssimLibrary(myLibraryName, session=mySession, overwrite = TRUE) 
+Definitions <- project(myLibrary, project="Definitions")
+
+#-------------------------------------------------------------------------------
+
+## DIAGNOSTICS 
+results <- read_rds("data/temp/stsim_run_results.rds")
+
+trans_results <- datasheet(myLibrary, scenario = results$scenarioId, 
+                           "stsim_OutputStratumTransition")
+# mun_set <- sample(trans_results$SecondaryStratumID, size = 20)
+
+print(head(trans_results))
+
+targets <- read_csv("config/stsim/TransitionTarget_10y.csv") %>% 
+  #filter(SecondaryStratumID %in% mun_set) %>% 
+  group_by(Timestep, SecondaryStratumID, TransitionGroupID) %>%
+  summarise(Amount_mean = mean(Amount)) %>% ungroup
+
+trans_results_mod <- trans_results  %>% 
+  #filter(SecondaryStratumID %in% mun_set) %>% 
+  group_by(Timestep, SecondaryStratumID, TransitionGroupID) %>%
+  summarise(Amount_mean = mean(Amount)) %>% ungroup
+trans_results_mod_only_3 <- trans_results_mod %>% 
+  filter(Timestep >= 3) %>% 
+  group_by(SecondaryStratumID, TransitionGroupID) %>%
+  summarise(Amount_mean = mean(Amount_mean)) %>% ungroup %>% 
+  mutate(Timestep=3)
+trans_results_toplot <- bind_rows(subset(trans_results_mod, Timestep < 3), 
+                                  trans_results_mod_only_3)
+
+# Plots 
+trans_results_toplot %>% 
+  filter(TransitionGroupID != "Urbanisation") %>% 
+  ggplot(aes(x=Timestep, y=Amount_mean)) +
+  geom_line(show.legend = F) + 
+  facet_grid_paginate(TransitionGroupID~SecondaryStratumID, nrow=3, ncol=5, page = 1, scales = "free") +
+  geom_line(data=targets, inherit.aes = T, linetype=2)
+ggsave("outputs/figures/one_to_one_mun.png") 
+
+## JOIN for general diagnoctic plot
+gen_plot_df <- left_join(trans_results_toplot, targets, 
+                         by= c("Timestep", "SecondaryStratumID", "TransitionGroupID")) %>% 
+  filter(TransitionGroupID != "Urbanisation") %>% 
+  arrange(Timestep)
+
+# general plot
+gen_plot_df %>% 
+  ggplot(aes(x=Amount_mean.x, Amount_mean.y, color=as.factor(Timestep))) +
+  geom_point(show.legend = F) +
+  #geom_abline() +
+  geom_abline(slope = 1, intercept = 0, linetype=2)
+ggsave("outputs/figures/one_to_one_targets.png")
+
+summary(lm(Amount_mean.y~Amount_mean.x, gen_plot_df))
