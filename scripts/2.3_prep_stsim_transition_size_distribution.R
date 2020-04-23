356a952808a7740621f8b1a06622912c5c5bad25
VLucet <valentin.lucet@gmail.com>
2020-04-23 18:35:36 -0400

    fix transition size distrib

--- scripts/2.3_prep_stsim_transition_size_distribution.R
+++ scripts/2.3_prep_stsim_transition_size_distribution.R
@@ -31,24 +31,14 @@ lu <- stack(raster("data/land_use/LandUse_mont_aafc_30by30_1990.tif"),
             raster("data/land_use/LandUse_mont_aafc_30by30_2010.tif"))
 
 lu_chg <- list(
-  urb = list(t1 = get_change_raster(raster_1 = lu$LandUse_mont_aafc_30by30_1990, 
-                                    raster_2 = lu$LandUse_mont_aafc_30by30_2000, 
-                                    class = 2, 
-                                    from = c(1,3), only_from=T), 
-             t2 = get_change_raster(raster_1 = lu$LandUse_mont_aafc_30by30_2000, 
-                                    raster_2 = lu$LandUse_mont_aafc_30by30_2010, 
-                                    class = 2, 
-                                    from = c(1,3), only_from=T)),
-  agex = list(t1 = get_change_raster(raster_1 = lu$LandUse_mont_aafc_30by30_1990, 
-                                     raster_2 = lu$LandUse_mont_aafc_30by30_2000, 
-                                     class = 1, 
-                                     from = 3, only_from = T), 
-              t2 = get_change_raster(raster_1 = lu$LandUse_mont_aafc_30by30_2000, 
-                                     raster_2 = lu$LandUse_mont_aafc_30by30_2010, 
-                                     class = 1, 
-                                     from = 3, only_from = T))
-)
-
+  urb =  get_change_raster(raster_1 = lu$LandUse_mont_aafc_30by30_1990, 
+                           raster_2 = lu$LandUse_mont_aafc_30by30_2010, 
+                           class = 2, 
+                           from = c(1,3), only_from=T),
+  agex =  get_change_raster(raster_1 = lu$LandUse_mont_aafc_30by30_1990, 
+                            raster_2 = lu$LandUse_mont_aafc_30by30_2010, 
+                            class = 1, 
+                            from = 3, only_from = T))
 
 # in R, slow but works
 
@@ -59,37 +49,44 @@ getPatchDist <- function(rast, gaps = F, # inputs for clump
   rast.clumped[rast.clumped == 0] <- NA
   pols <- st_as_sf(rasterToPolygons(rast.clumped, dissolve = T))
   areas <- as.numeric(st_area(pols))
-  return(list(areas=areas, rasters=rast.clumped))
+  return(list(areas=areas, rasters=rast.clumped, pols=pols))
 }
 
 # Get areas for each time step possible
 all_areas <- list()
 
-for (dim1 in c("urb", "agex")){
+for (class in c("urb", "agex")){
   
-  for (dim2 in c("t1", "t2")){
-    areas <-  getPatchDist(lu_chg[[dim1]][[dim2]])$areas
-    areas.df <- as.data.frame(table(sort(areas/10000)))
-    areas.df$Var1 <- ceiling(as.numeric(as.character(areas.df$Var1)))
-    names(areas.df) <- c("MaximumArea", "Amount")
-    df <- data.frame(areas.df,
-                     Timestep = dim2,
-                     Transition = dim1, 
-                     stringsAsFactors = FALSE)
-    
-    all_areas <- list.append(all_areas, df)
-    #str(all_areas)
-  }
+  res <-  getPatchDist(lu_chg[[class]])
+  areas <-  res$areas
+  areas.df <- as.data.frame(table(sort(areas/10000)))
+  areas.df$Var1 <- ceiling(as.numeric(as.character(areas.df$Var1)))
+  names(areas.df) <- c("MaximumArea", "Amount")
+  df <- data.frame(areas.df,
+                   TransitionGroupID = class, 
+                   stringsAsFactors = FALSE)
   
+  all_areas <- list.append(all_areas, df)
+  #str(all_areas)
 }
 
 all_areas_df <- bind_rows(all_areas)
 
 # Reformat the df
-df_urban_change <- all_areas_df %>% 
-  group_by(Transition, Timestep, MaximumArea) %>% 
-  summarise(Amount = sum(Amount)) %>% 
-  mutate(RelativeAmount = Amount/sum(Amount))
+df_change <- all_areas_df %>% 
+  group_by(TransitionGroupID, MaximumArea) %>% 
+  summarise(Amount = sum(Amount)) %>%
+  mutate(RelativeAmount = Amount/sum(Amount)) %>% 
+  select(-Amount) %>% ungroup() %>% 
+  mutate(group = cut(MaximumArea, breaks=c(0,1,5,100,1000))) %>% 
+  group_by(TransitionGroupID, group) %>% 
+  summarise(RelativeAmount=sum(RelativeAmount), MaximumArea=max(MaximumArea)) %>% 
+  select(-group) %>% ungroup()
+  
+df_change$TransitionGroupID[df_change$TransitionGroupID=="urb"] <- 
+  "Urbanisation"
+df_change$TransitionGroupID[df_change$TransitionGroupID=="agex"] <- 
+  "Agricultural Expansion [Type]"
 
-write.csv(df_urban_change, "config/stsim/TransitionSizeDistribution.csv", 
+write.csv(df_change, "config/stsim/TransitionSizeDistribution.csv", 
           row.names = F)
