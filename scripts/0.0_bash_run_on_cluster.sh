581cab8f0bf32848b8455848b07bf031c1aba921
VLucet <valentin.lucet@gmail.com>
2020-04-27 18:35:13 -0400

    removes traps - didnt work

--- scripts/0.0_bash_run_on_cluster.sh
+++ scripts/0.0_bash_run_on_cluster.sh
@@ -3,11 +3,6 @@
 # exit when any command fails
 set -e
 
-# keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
-
 # reviewed 2020 
 
 # -- for cedar
