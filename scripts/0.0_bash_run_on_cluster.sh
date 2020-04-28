4f98b8845b3da4015ff9c9cab1da1ccc9c8e5db7
VLucet <valentin.lucet@gmail.com>
2020-04-27 23:38:55 -0400

    updtaes nb of iterations

--- scripts/0.0_bash_run_on_cluster.sh
+++ scripts/0.0_bash_run_on_cluster.sh
@@ -38,7 +38,7 @@ export R_RATIO='2'
 export R_METHOD_STSIM='rf'
 export R_SAMPLING_METHOD="bal"
 # Variables for STSIM
-export STSIM_ITER='2'
+export STSIM_ITER='10'
 export STSIM_TS_START='0'
 export STSIM_TS_END='5'
 export STSIM_STEP_SAVE='1'
