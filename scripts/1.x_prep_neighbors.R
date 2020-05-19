62114a935d4055162591d0630ccf887f6d2c2ff8
VLucet <valentin.lucet@gmail.com>
2020-05-19 11:56:49 -0400

    change numbering

--- scripts/1.x_prep_neighbors.R
+++ scripts/1.x_prep_neighbors.R
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-## 1.5 Prep Neighbors
+## 1.x Prep Neighbors
 ## 2020
 ## Inputs: Land use data
 ## Outputs: Reclassified land use data
