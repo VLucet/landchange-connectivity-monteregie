7a092ff5b487575e71830ef1b26ba37545744c4f
VLucet <valentin.lucet@gmail.com>
2020-07-09 10:55:04 -0400

    change alpha

--- scripts/8.2_process_SURF_output.R
+++ scripts/8.2_process_SURF_output.R
@@ -5,6 +5,6 @@ print(head(surf))
 surf %>% 
   mutate(scenario = as.factor(scenario)) %>% 
   ggplot(aes(x = timestep, y = kp_nb, color=scenario)) +
-  geom_smooth(aes(group = scenario), alpha=0.1, method="loess")+
+  geom_smooth(aes(group = scenario), alpha=0.5, method="loess")+
   geom_point(aes(group=scenario))+
   facet_wrap(~species, scales = "free")
