e90b2fe0ca66f97df2753dbb37ed8d027b1be277
VLucet <valentin.lucet@gmail.com>
2020-07-09 15:52:34 -0400

    new img process function to test

--- surf/utils.py
+++ surf/utils.py
@@ -32,18 +32,55 @@ def read_img(img):
     return img
 
 
-# Process image
-def process_img(img, in_range=(1, 20), out_range=(0, 255)):
-    import cv2
-    from skimage.exposure import rescale_intensity
-    img = read_img(img)
+# Scale the image
+def scale_img(img):
     img_scaled = (img * 255.0 / img.max())
-    img_processed = rescale_intensity(img_scaled, in_range=in_range, out_range=out_range).astype("uint8")
+    return img_scaled
+
+
+def transform_img(img):
+    import numpy as np
+    img_db = 10 * np.log10(img)
+    img_db_pos = img_db + abs(np.min(img_db))
+    return img_db_pos
+
+
+def get_peak_img(img):
+    import numpy as np
+    from scipy import signal
+
+    data = img.flatten()
+    n, bins = np.histogram(data, 100)
+
+    # trim data
+    x = np.linspace(np.min(data), np.max(data), num=100)
+
+    # find index of minimum between two modes
+    ind_max = signal.argrelmax(n)
+    x_max = x[ind_max]
+    y_max = n[ind_max]
+
+    # plot
+    # plt.hist(data, bins=100, color='y')
+    # plt.scatter(x_max, y_max, color='b')
+    # plt.show()
+
+    y_main_peak = np.max(y_max)
+    x_main_peak = x_max[np.argmax(y_max)]
+    return x_main_peak, y_main_peak
+
+
+# Process image
+def process_img(img):
+    from skimage.exposure import rescale_intensity
+    img_scaled = scale_img(transform_img(read_img(img)))
+    x, y = get_peak_img(img_scaled)
+    img_processed = rescale_intensity(img_scaled, (x-30, x+30), (0, 255)).astype("uint8")
     return img_processed
 
 
 # Customized version of SURF algorithm from cv2, returns an annotated image
-def surf_detect(img, h_threshold=1000, oct_layers=3, oct_nb=4, upright=False, verbose=False, kp_only=True):
+def surf_detect(img, h_threshold=20000, oct_layers=3, oct_nb=3, upright=False, verbose=False, kp_only=False):
     import cv2
     # Read image
     if isinstance(img, str):
@@ -69,9 +106,8 @@ def surf_detect(img, h_threshold=1000, oct_layers=3, oct_nb=4, upright=False, ve
 
 
 # Process flow, combine all functions
-def process_flow(img, in_range=(2, 20), out_range=(0, 255), h_threshold=30000,
-                 oct_layers=5, oct_nb=5, upright=True, verbose=False, kp_only=False):
-    img_processed = process_img(img, in_range, out_range)
+def process_flow(img, h_threshold=20000, oct_layers=3, oct_nb=3, upright=False, verbose=False, kp_only=False):
+    img_processed = process_img(img)
     img_annotated = surf_detect(img_processed, h_threshold, oct_layers, oct_nb, upright, verbose, kp_only)
     return img_annotated
 
@@ -80,6 +116,11 @@ def get_kp_lengths(img):
     img_annotated = process_flow(img)
     the_length = len(img_annotated.kp)
     return the_length
+
+
+def process_and_plot(img):
+    img_annotated = process_flow(img)
+    img_annotated.plot()
 
 
 # ------ Plotting functions ------
