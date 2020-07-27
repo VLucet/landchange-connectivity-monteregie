4ff2cf54271af42573ad5a79b3d2f3cc55a1b493
VLucet <valentin.lucet@gmail.com>
2020-07-27 16:40:15 -0400

    udpates

--- scripts/8.3_generate_package_biblio.R
+++ scripts/8.3_generate_package_biblio.R
@@ -1,11 +1,11 @@
 # From https://github.com/rstudio/renv/issues/340
 
 renvLock <- jsonlite::read_json("renv.lock")
-sink("../msc-thesis-mcgill/r_packges.bib")
+sink("../msc-thesis-mcgill/r_packages.bib")
 knitr::write_bib(names(renvLock$Packages))
 sink()
 
 renvLock <- jsonlite::read_json("renv.lock")
-sink("/r_packges.bib")
+sink("/r_packages.bib")
 knitr::write_bib(names(renvLock$Packages))
 sink()
