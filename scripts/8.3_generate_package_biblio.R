ea10bb50db02f3dc6ee30f373a5d0e468466b804
VLucet <valentin.lucet@gmail.com>
2020-07-31 21:23:55 -0400

    updates

--- scripts/8.3_generate_package_biblio.R
+++ scripts/8.3_generate_package_biblio.R
@@ -1,6 +1,6 @@
 # From https://github.com/rstudio/renv/issues/340
 
 renvLock <- jsonlite::read_json("renv.lock")
-sink("r_packages.bib")
+sink("bib/r_packages.bib")
 knitr::write_bib(names(renvLock$Packages))
 sink()
