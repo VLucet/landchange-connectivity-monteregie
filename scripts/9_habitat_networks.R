24895f3856d2bbea63fd4bfcf8f229893713902c
Valentin <valentin.lucet@gmail.com>
2021-03-30 12:05:47 -0400

    RE

--- scripts/9_habitat_networks.R
+++ scripts/9_habitat_networks.R
@@ -14,7 +14,7 @@ library(doParallel)
 # Load file list in patches
 
 # media_path <- "/media/vlucet/backup/results_march_2021"
-media_path <- "/home/rstudio/land_con_monteregie"
+media_path <- "/home/rstudio/land_con_monteregie/outputs"
 
 patches <- list.files(file.path(media_path, "hab_suit/"), 
                       pattern = ".tif") # DOES NOT HAVE TRUE
